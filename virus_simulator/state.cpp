de4ed6d9b096de838eecba8ed14a1d778755b3e7
YuYoungGyu <54985877+YuYoungGyu@users.noreply.github.com>
2021-05-17 15:27:03 +0900

    Update state.cpp

--- virus_simulator/state.cpp
+++ virus_simulator/state.cpp
@@ -26,18 +26,19 @@ enum statement{
 class Person{
     private:
         static int time;
-        int check;
+        int recover_try;
 
         vector<Person*> near_node;
         static int recovered_person;
         static int suspected_person;
         static int infected_person;
+        static int removed_person;
         statement state;
     public:
         Person(){
             state = Suspected;
             suspected_person++;
-            check = time;
+            recover_try = 0;
         }
         statement get_state(){
             return state;
@@ -51,31 +52,58 @@ class Person{
         int get_infected_person(){
             return infected_person;
         }
+        int get_recover_try(){
+            return recover_try;
+        }
         void connect(Person* p){
             this->near_node.push_back(p);
             p->near_node.push_back(this);
         }
-        void infect(){
-            state = Infected;
-            suspected_person--;
-            infected_person++;
-        }
-        void recover(){
-            state = Recovered;
-            infected_person--;
-            recovered_person++;
-        }
+        bool infect(int infected_per){
+            int random = rand()%100;
 
-        int display_near_node(){
+            if(random < infected_per) { 
+                state = Infected;
+                suspected_person--;
+                infected_person++;
+                return true;
+            }
+            else return false;
+        }
+        bool recover(int recovered_per){
+            int random = rand()%100;
+            recover_try++;
+            if(random < recovered_per) { 
+                state = Recovered;
+                infected_person--;
+                recovered_person++;
+                return true;
+            }
+            else return false;
+        }
+        void remove(){
+            state = Removed;
+            infected_person--;
+            removed_person++;
+        }
+        void display_near_node(){
             int result = near_node.size();
             cout << result << endl;
-            return result;
+            
         }
         void time_increment(){
             time++;
         }
-        void set_check(){
-            check = time;
+        vector<Person*> get_near_node(){
+            return near_node;
+        }
+        void simulation_display(){
+            cout << "TIME: " << time << endl;
+            cout << "Suspected_person: " << suspected_person << "명 입니다." << endl;
+            cout << "Infected_person : " << infected_person  << "명 입니다." << endl;
+            cout << "Recovered_person: " << recovered_person << "명 입니다." << endl;
+            cout << "removed_person  : " << removed_person   << "명 입니다." << endl;
+            cout << "----------------------------------------" << endl;
         }
 };
 
@@ -83,9 +111,9 @@ int Person::time = 0;
 int Person::recovered_person = 0;
 int Person::suspected_person = 0;
 int Person::infected_person = 0;
+int Person::removed_person = 0;
 
 pair<int,int> select_connect_node(vector<int> pool){
-    srand((unsigned int)time(NULL));
     int s = pool.size();
     int idx1 = rand()%s;
     int idx2 = rand()%s;
@@ -99,6 +127,8 @@ pair<int,int> select_connect_node(vector<int> pool){
 }
 
 int main(){
+    srand((unsigned int)time(NULL));
+
     vector<Person*> network;
     vector<int> pool;
     pair<int,int> selected_idx;
@@ -135,31 +165,41 @@ int main(){
         pool.push_back(pool[idx1]);
         pool.push_back(pool[idx2]);
         pool.push_back(s-1);
-        pool.push_back(s-1);
-        
+        pool.push_back(s-1);   
     }
 
     Person* start;
     int tmp_idx = 0;
     for(Person* node: network){
-        int size = node->display_near_node();
+        int size = node->get_near_node().size();
         if(tmp_idx < size){
             start = node;
             tmp_idx = size;
         }
     }
-    start->display_near_node();
 
     // 바이러스 숙주 한명 선택
-    start->infect();
+    start->infect(100);
     start->time_increment();
-    start->set_check();
 
     vector<Person*> infected_vector;
+    vector<Person*> newly_infected;
+    
     infected_vector.push_back(start);
 
     while(!infected_vector.empty()){
+        infected_vector.back()->simulation_display();
+        infected_vector.back()->time_increment();
         for(Person* infected_person: infected_vector){
+            for(Person* near_person: infected_person->get_near_node()){
+                if(near_person->get_state() == Suspected && near_person->infect(infected_per)) {
+                    newly_infected.push_back(near_person);
+                }
+                
+            }
+            // 회복 시도한다.
+            infected_person->recover(recovered_per);
+            // 일정 시도를 넘기면 죽는다.
             
         }
     }
